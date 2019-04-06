f27914fcde031b42aa19cad352d718c9293452d4
fernandoms <marinhosilva.fernando@gmail.com>
2019-04-06 17:39:39 +0100

    spa script

---
+++ src/client/build/precache-manifest.73d0326f9bb138dba8ecb55c4555d6dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "01b45b2c1c9f2936ecbf",
+    "url": "/static/js/main.01b45b2c.chunk.js"
+  },
+  {
+    "revision": "a6eed4a2d23a7c5e7a61",
+    "url": "/static/js/2.a6eed4a2.chunk.js"
+  },
+  {
+    "revision": "01b45b2c1c9f2936ecbf",
+    "url": "/static/css/main.b866ffb6.chunk.css"
+  },
+  {
+    "revision": "a6eed4a2d23a7c5e7a61",
+    "url": "/static/css/2.6112ebcd.chunk.css"
+  },
+  {
+    "revision": "2d51b0267dc27028cebc1813153ebfe2",
+    "url": "/index.html"
+  }
+];
