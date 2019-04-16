978a4f2397501692fed915f254a6332c3dcac966
rbsrafa <rbsrafa@gmail.com>
2019-04-16 10:03:39 +0100

    added terms of service page

---
+++ src/client/src/app/pages/termsOfService/termsOfService.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TermsOfService extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <script id="CookieDeclaration" src="https://consent.cookiebot.com/a01b2eb7-0b0b-403a-98af-2d7bfd8c8f2c/cd.js" type="text/javascript" async></script>
+      </React.Fragment>
+    )
+  }
+}
