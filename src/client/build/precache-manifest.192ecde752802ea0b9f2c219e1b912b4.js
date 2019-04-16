a0b624225b21c6e405f582337929553a4c592d91
rbsrafa <rbsrafa@gmail.com>
2019-04-16 10:19:23 +0100

    added build

---
+++ src/client/build/precache-manifest.192ecde752802ea0b9f2c219e1b912b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9cf5e01785e9e18c6811",
+    "url": "/static/js/main.9cf5e017.chunk.js"
+  },
+  {
+    "revision": "830ed7d4697b4040bfb0",
+    "url": "/static/js/2.830ed7d4.chunk.js"
+  },
+  {
+    "revision": "9cf5e01785e9e18c6811",
+    "url": "/static/css/main.e5f7e9d2.chunk.css"
+  },
+  {
+    "revision": "830ed7d4697b4040bfb0",
+    "url": "/static/css/2.d7598d57.chunk.css"
+  },
+  {
+    "revision": "080c2ff5f29e2962503ffdefca525ac3",
+    "url": "/index.html"
+  }
+];
