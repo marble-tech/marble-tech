cbe11b84fb9d5d405d7f3cd39f2c0553d8676548
rbsrafa <rbsrafa@gmail.com>
2019-04-20 13:13:09 +0100

    populated footer

---
+++ src/client/build/precache-manifest.a74ff3cb4820409e8bd246f28c07a19a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6d702a8dfe42a6939a3b",
+    "url": "/static/js/main.6d702a8d.chunk.js"
+  },
+  {
+    "revision": "6c29b9816694aef86afb",
+    "url": "/static/js/2.6c29b981.chunk.js"
+  },
+  {
+    "revision": "6d702a8dfe42a6939a3b",
+    "url": "/static/css/main.85fc60b0.chunk.css"
+  },
+  {
+    "revision": "6c29b9816694aef86afb",
+    "url": "/static/css/2.d7598d57.chunk.css"
+  },
+  {
+    "revision": "9af6d004cceb97c3449e8aac9ecbd4df",
+    "url": "/index.html"
+  }
+];
--- src/client/build/service-worker.js
+++ src/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e7a0bb10527cb8fb149353750ca2bc8.js"
+  "/precache-manifest.a74ff3cb4820409e8bd246f28c07a19a.js"
 );
 
 workbox.clientsClaim();
