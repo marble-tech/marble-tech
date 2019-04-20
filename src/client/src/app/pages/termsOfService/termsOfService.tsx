c621c5d7827acb2aa847d95210184ba758cec0ea
rbsrafa <rbsrafa@gmail.com>
2019-04-20 10:21:10 +0100

    test terms of service

--- src/client/src/app/pages/termsOfService/termsOfService.tsx
+++ src/client/src/app/pages/termsOfService/termsOfService.tsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export default class TermsOfService extends Component {
+
+  componentDidMount(){
+    const script = document.createElement("script");
+    script.src = "https://consent.cookiebot.com/a01b2eb7-0b0b-403a-98af-2d7bfd8c8f2c/cd.js";
+    script.id = "CookieDeclaration";
+    script.async = true;
+    document.head.appendChild(script);
+  }
+
   render() {
     return (
       <React.Fragment>
-        <script id="CookieDeclaration" src="https://consent.cookiebot.com/a01b2eb7-0b0b-403a-98af-2d7bfd8c8f2c/cd.js" type="text/javascript" async></script>
+        
       </React.Fragment>
     )
   }
