6358a049f5b318731e9af7b16c98c817ea7574ab
rbsrafa <rbsrafa@gmail.com>
2019-05-06 18:40:30 +0100

    added master build

---
+++ src/client/build/precache-manifest.dad65947ee38eba939c94846b43b5495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "641f3710124b0bb73058",
+    "url": "/static/js/main.641f3710.chunk.js"
+  },
+  {
+    "revision": "ff1dfe403d11816a927c",
+    "url": "/static/js/2.ff1dfe40.chunk.js"
+  },
+  {
+    "revision": "641f3710124b0bb73058",
+    "url": "/static/css/main.dc520395.chunk.css"
+  },
+  {
+    "revision": "ff1dfe403d11816a927c",
+    "url": "/static/css/2.ab33ccd6.chunk.css"
+  },
+  {
+    "revision": "2be30ec58332ca769d5a57789d437906",
+    "url": "/index.html"
+  }
+];
